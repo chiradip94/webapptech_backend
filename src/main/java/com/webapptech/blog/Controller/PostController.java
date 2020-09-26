f6254b9d203ce7fa04e75666e6c29dc5cf3e589b
Rajdeep Maji <rajdeep.svts@gmail.com>
2020-09-26 23:47:18 +0530

    added cors

--- src/main/java/com/webapptech/blog/Controller/PostController.java
+++ src/main/java/com/webapptech/blog/Controller/PostController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.webapptech.blog.Model.Post;
 import com.webapptech.blog.Service.PostService;
 
+@CrossOrigin(origins = { "*" }, maxAge = 3000)
 @RestController
 public class PostController {
 	
