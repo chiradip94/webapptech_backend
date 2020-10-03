704f073b76cd6642ece40bd690dcc6b4249bb941
Rajdeep Maji <rajdeep.svts@gmail.com>
2020-10-03 17:13:10 +0530

    changed add comment to request body

---
+++ src/main/java/com/webapptech/blog/DTO/CommentRequest.java
@@ -0,0 +1,18 @@
+package com.webapptech.blog.DTO;
+
+public class CommentRequest {
+	private int id;
+	private String comment;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+}
